3f51631f3ff9ad84921f38242f490bd60e2c00b0
sdtsui <danielt213@gmail.com>
2015-11-26 00:34:45 -0800

    app.js entry point for babel

---
+++ src/app.js
@@ -0,0 +1,8 @@
+'use strict'
+import * as __D from 'constants.js';
+import * as Dashboard from 'Dashboard.js';
+import * as Sith from 'Sith.js';
+import * as SithList from 'SithList.js';
+
+console.log(Dashboard);
+
